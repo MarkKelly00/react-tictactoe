3942ed0b0949fc59e9f5ed0467249f1f031c0565
Mako3 <big.mako@gmail.com>
2021-06-12 00:41:14 -0700

    updated styles and added title

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 import Game from './components/Game';
 
 function App() {
   return (
+    <>
+    <h1 className="title">Tic Tac Toe</h1>
     < Game />
+    </>
   );
 }
 
